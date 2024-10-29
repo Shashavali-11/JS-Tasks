4c7c6752412ab3f842e73885cd1b4ba6f8839bd2
Shashavali <109945362+Shashavali-11@users.noreply.github.com>
2024-10-29 10:59:25 +0530

    Update task9.js
    Update

--- task9.js
+++ task9.js
@@ -1,32 +1,32 @@
 // 1) Question ?
 // Voting Eligibility
-// var age1 = 21;
-// var citizenship = true;
-// if (age1 >= 18 && citizenship) {
-//   console.log(`You can Vote...`);
-// } else {
-//   console.log(`You cannot Vote...`);
-// }
+var age1 = 21;
+var citizenship = true;
+if (age1 >= 18 && citizenship) {
+  console.log(`You can Vote...`);
+} else {
+  console.log(`You cannot Vote...`);
+}
 
 // 2) Question ?
 // Admission to a club
-// var age2 = 21;
-// var hasInvitation = true;
-// if (age2 >= 21 || hasInvitation) {
-//   console.log(`Can Enter into Club`);
-// } else {
-//   console.log(`Cannot Enter Club`);
-// }
+var age2 = 21;
+var hasInvitation = true;
+if (age2 >= 21 || hasInvitation) {
+  console.log(`Can Enter into Club`);
+} else {
+  console.log(`Cannot Enter Club`);
+}
 
 // 3) Question ?
 // Discount Eligibility
-// var age3 = 55;
-// var isMember = false;
-// if (age3 >= 65 || isMember) {
-//   console.log(`You are Eligible for Discount...`);
-// } else {
-//   console.log(`You are Not Eligible for Discount...`);
-// }
+var age3 = 55;
+var isMember = false;
+if (age3 >= 65 || isMember) {
+  console.log(`You are Eligible for Discount...`);
+} else {
+  console.log(`You are Not Eligible for Discount...`);
+}
 
 //   4) Question ?
 // Scholarship Eligibility
