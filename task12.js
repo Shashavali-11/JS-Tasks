de12a678e53c1e33d28f4b9af75fc573313e62c6
Shashavali <109945362+Shashavali-11@users.noreply.github.com>
2024-11-06 10:45:25 +0530

    Update task12.js
    Updated with outputs

--- task12.js
+++ task12.js
@@ -1,6 +1,6 @@
 //  1) Write a function to check wheather input num is even or odd ?
 var number = 11;
-even(number)
+even(number)  // 11 is an odd number.
 function even(num){
     if(num%2 == 0){
         console.log(`${num} is an even number.`)
@@ -13,7 +13,7 @@ function even(num){
 
 //  2) Write a function to check wheather input string is palindrome or not ?
 var string = "MOOM"
-palindrome(string)
+palindrome(string) // MOOM is a Palindrome.
 function palindrome(str){
     var newstr = ""
     for(i=str.length-1; i>=0; i--){
@@ -32,7 +32,7 @@ function palindrome(str){
 var number1 = 70;
 var number2 = 90;
 var number3 = 130;
-max(number1, number2, number3)
+max(number1, number2, number3) // 130 is a Maximum.
 function max(num1, num2, num3){
     if((num1>num2) && (num1>num3)){
         console.log(`${num1} is Maximum.`)
