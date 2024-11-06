bc74bf5c132f1848e751bade0d5a5f8ea4254ec2
Shashavali <109945362+Shashavali-11@users.noreply.github.com>
2024-11-06 10:42:24 +0530

    Add files via upload
    Functions

---
+++ task12.js
@@ -0,0 +1,45 @@
+//  1) Write a function to check wheather input num is even or odd ?
+var number = 11;
+even(number)
+function even(num){
+    if(num%2 == 0){
+        console.log(`${num} is an even number.`)
+    }else{
+        console.log(`${num} is an odd number.`)
+    }
+}
+// ------------------------------------------------------------------------------------------------>
+
+
+//  2) Write a function to check wheather input string is palindrome or not ?
+var string = "MOOM"
+palindrome(string)
+function palindrome(str){
+    var newstr = ""
+    for(i=str.length-1; i>=0; i--){
+        newstr += str[i];
+    }
+    if(str == newstr){
+        console.log(`${str} is a Palindrome. `)
+    }else{
+        console.log(`${str} is not a Palindrome. `)
+    }
+}
+// ------------------------------------------------------------------------------------------------>
+
+
+//  3) Write a function to check largest num among three numbers ?
+var number1 = 70;
+var number2 = 90;
+var number3 = 130;
+max(number1, number2, number3)
+function max(num1, num2, num3){
+    if((num1>num2) && (num1>num3)){
+        console.log(`${num1} is Maximum.`)
+    }else if((num2>num1)&&(num2>num3)){
+        console.log(`${num2} is Maximum.`)
+    }else{
+        console.log(`${num3} is Maximum.`)
+    }
+}
+
